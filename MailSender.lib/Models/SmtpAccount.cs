1e4859c59ecbbb8170e04fabf74e14e3a613b39c
Олег Нестеров <natol6@mail.ru>
2021-02-17 19:05:31 +0300

    В библиотеке MailSender.lib создана папка для модели, в папку добавлены классы сущностей модели. В папку для сервисов добавлены пустые классы сервисов

---
+++ MailSender.lib/Models/SmtpAccount.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MailSender.lib.Models
+{
+    public class SmtpAccount : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private int _Id;
+        public int Id
+        {
+            get => _Id;
+            set
+            {
+                _Id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        private string _Login;
+        public string Login
+        {
+            get => _Login;
+            set
+            {
+                _Login = value;
+                OnPropertyChanged("Login");
+            }
+        }
+        private string _Password;
+        public string Password
+        {
+            get => _Password;
+            set
+            {
+                _Password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+        private string _Person;
+        public string Person
+        {
+            get => _Person;
+            set
+            {
+                _Person = value;
+                OnPropertyChanged("Person");
+            }
+        }
+        private int _SmtpServerid;
+        public int SmtpServerId
+        {
+            get => _SmtpServerid;
+            set
+            {
+                _SmtpServerid = value;
+                OnPropertyChanged("MailServiceId");
+            }
+        }
+        private void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
