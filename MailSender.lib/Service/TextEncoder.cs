fc44a29753365d5c17ce80fcd0321ee410666263
Олег Нестеров <natol6@mail.ru>
2021-02-17 03:09:12 +0300

    В проекте MailSender установлен пакет FontAwesome5. Создана библиотека классов MailSender.lib, в библиотеку добавлен статический класс с методами для кодирования и раскодирования пароля

---
+++ MailSender.lib/Service/TextEncoder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender.lib.Service
+{
+    public static class TextEncoder
+    {
+        public static string Encode(SecureString str, int key = 1)
+        {
+            string pas = new System.Net.NetworkCredential(string.Empty, str).Password;
+            return new (pas.Select(c => (char) (c + key)).ToArray());
+        }
+        public static SecureString Decode(string str, int key = 1)
+        {
+            using SecureString sstr = new SecureString();
+            sstr.Clear();
+            foreach(char c in str)
+            {
+                sstr.AppendChar((char)(c - key));
+            }
+            return sstr;
+        }
+    }
+}
